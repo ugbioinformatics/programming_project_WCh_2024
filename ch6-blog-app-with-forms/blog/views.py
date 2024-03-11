17de15535cd1eb13bdf99f070b0e32b463b56e6d
Cezary Czaplewski <czarek@etoh.chem.ug.edu.pl>
2024-03-11 15:41:29 +0100

    ch6-blog-app-with-forms

---
+++ ch6-blog-app-with-forms/blog/views.py
@@ -0,0 +1,44 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView  # new
+from django.urls import reverse_lazy  # new
+from django.shortcuts import get_object_or_404, render
+
+
+from .models import Post
+
+def suma(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    tmp=post.liczby.split()
+    for i in range(0, len(tmp)):
+        tmp[i]=int(tmp[i])  
+    post.suma=sum(tmp)
+    post.save()
+    return render(request, 'post_detail.html', {'post': post})
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body", "liczby"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body", "liczby"]
+
+
+class BlogDeleteView(DeleteView):  # new
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")

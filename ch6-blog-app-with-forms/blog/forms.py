f8f5841be66cdb3e0c7203e2a640f1bcce80c4d7
Cezary Czaplewski <100878859+cc1781@users.noreply.github.com>
2024-03-11 17:48:34 +0100

    Update forms.py

--- ch6-blog-app-with-forms/blog/forms.py
+++ ch6-blog-app-with-forms/blog/forms.py
@@ -3,3 +3,4 @@ from .models import Post
 
 class Suma(forms.Form):
     body = forms.CharField(max_length=40,widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
+    data1 = forms.DateField()

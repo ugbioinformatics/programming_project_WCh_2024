7a6ee126a886bcf4aa61065c7e5ebc5943cf7640
Cezary Czaplewski <100878859+cc1781@users.noreply.github.com>
2024-03-11 17:58:53 +0100

    Update forms.py

--- ch6-blog-app-with-forms/blog/forms.py
+++ ch6-blog-app-with-forms/blog/forms.py
@@ -4,4 +4,4 @@ import datetime
 
 class Suma(forms.Form):
     body = forms.CharField(max_length=40,widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
-    data1 = forms.DateField(initial=datetime.date.today,label="Podaj datę",help_test="data obliczeń")
+    data1 = forms.DateField(initial=datetime.date.today,label="Podaj datę",help_text="data obliczeń")

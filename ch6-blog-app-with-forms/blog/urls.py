a45622e5f960cb7ba51dbda1549580c39db9b921
Cezary Czaplewski <100878859+cc1781@users.noreply.github.com>
2024-03-11 16:17:35 +0100

    Update urls.py

--- ch6-blog-app-with-forms/blog/urls.py
+++ ch6-blog-app-with-forms/blog/urls.py
@@ -11,7 +11,7 @@ from .views import (
 urlpatterns = [
     path("post/new/", BlogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
-    path("post/<int:pk>/suma", suma, name="post_suma"),    
+    path("post/suma", suma, name="post_suma"),    
     path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
     path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),  # new
     path("", BlogListView.as_view(), name="home"),

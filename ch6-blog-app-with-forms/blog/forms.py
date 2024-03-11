01f271aa33860e6be1e507de2869700f1af022ee
tugnt12 <153234784+tugnt12@users.noreply.github.com>
2024-03-11 18:27:53 +0100

    Update forms.py

--- ch6-blog-app-with-forms/blog/forms.py
+++ ch6-blog-app-with-forms/blog/forms.py
@@ -6,3 +6,4 @@ class Suma(forms.Form):
     body = forms.CharField(label="Dane",help_text="liczby do sumowania",max_length=40,widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
     data1 = forms.DateField(initial=datetime.date.today,label="Podaj datę",help_text="data obliczeń")
     URL = forms.URLField()
+    bool = forms.BooleanField()

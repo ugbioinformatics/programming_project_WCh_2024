6ccc4ac4f1b614e2df38c96b00e4168815ef2a7c
Cezary Czaplewski <100878859+cc1781@users.noreply.github.com>
2024-03-11 18:23:19 +0100

    Update forms.py

--- ch6-blog-app-with-forms/blog/forms.py
+++ ch6-blog-app-with-forms/blog/forms.py
@@ -3,6 +3,6 @@ from .models import Post
 import datetime
 
 class Suma(forms.Form):
-    body = forms.CharField(max_length=40,widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
+    body = forms.CharField(label="Dane",help_text="liczby do sumowania",max_length=40,widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
     data1 = forms.DateField(initial=datetime.date.today,label="Podaj datę",help_text="data obliczeń")
     URL = forms.URLField()

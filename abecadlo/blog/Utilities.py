b971fc6a3eb98601d33548ec6d00337eec1b08b1
Sieniek02 <127025898+Sieniek02@users.noreply.github.com>
2024-03-18 17:30:37 +0100

    Create Utilities.py

---
+++ abecadlo/blog/Utilities.py
@@ -0,0 +1,17 @@
+def make_png_and_mop(smiles):
+    czasteczka = openbabel.pybel.readstring("smi", smiles)
+    czasteczka.write(format="_png2",filename="molecule.png", overwrite=True)
+    czasteczka.make3D()
+    czasteczka.write(format="mop",filename="molecule.mop",overwrite=True)
+
+def CIRconvert(ids):
+  from urllib.request import urlopen
+  from urllib.parse import quote
+  try:
+        url = 'http://cactus.nci.nih.gov/chemical/structure/' + quote(ids) + '/smiles'
+    
+        ans = urlopen(url).read().decode('utf8')
+        for line in ans:
+            return ans
+    except:
+        return 'Did not work'

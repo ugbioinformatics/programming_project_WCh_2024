8615a7fd11ccf757680ee941acb2d2e0bdd4716c
Sieniek02 <127025898+Sieniek02@users.noreply.github.com>
2024-03-18 16:50:13 +0100

    Update forms.py

--- abecadlo/blog/forms.py
+++ abecadlo/blog/forms.py
@@ -3,7 +3,8 @@ from .models import Post
 import datetime
 
 class Suma(forms.Form):
-    body = forms.CharField(label="Dane",help_text="liczby do sumowania",max_length=40,widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
-    data1 = forms.DateField(initial=datetime.date.today,label="Podaj datę",help_text="data obliczeń")
-    URL = forms.URLField()
-    bool = forms.BooleanField()
+    pole_nazwa = forms.CharField(widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
+    pole_smiles = forms.CharField(widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
+    #data1 = forms.DateField(initial=datetime.date.today,label="Podaj datę",help_text="data obliczeń")
+    #URL = forms.URLField()
+    #bool = forms.BooleanField()

281c9ef9e1ffdd7c9b4ed3d2c698379c140de318
j-kolakow <160509285+j-kolakow@users.noreply.github.com>
2024-03-18 17:11:45 +0100

    Update smile_to_picFunk.py

--- smile_to_picFunk.py
+++ smile_to_picFunk.py
@@ -1,4 +1,5 @@
 def make_png_and_mop(smiles):
     czasteczka = openbabel.pybel.readstring("smi", smiles)
     czasteczka.write(format="_png2",filename="molecule.png", overwrite=True)
+    czasteczka.make3D()
     czasteczka.write(format="mop",filename="molecule.mop",overwrite=True)

300c47c13005cd6e5e7485748ffe2c8f5f9bd427
Sieniek02 <127025898+Sieniek02@users.noreply.github.com>
2024-03-18 17:16:19 +0100

    Update forms.py

--- abecadlo/blog/forms.py
+++ abecadlo/blog/forms.py
@@ -3,16 +3,16 @@ from .models import Post
 import datetime
 
 class Suma(forms.Form):
-    pole_nazwa = forms.CharField(widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
-    pole_smiles = forms.CharField(widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
+    pole_nazwa = forms.CharField(required = False,widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
+    pole_smiles = forms.CharField(required = False,widget=forms.TextInput(attrs={'size':40, 'maxlength':40}))
     #data1 = forms.DateField(initial=datetime.date.today,label="Podaj datę",help_text="data obliczeń")
     #URL = forms.URLField()
     #bool = forms.BooleanField()
 
 def clean(self):
     cleaned_data = super(Suma, self).clean()
-    pole_nazwa = cleaned_data.get(pole_nazwa)
-    pole_smiles = cleaned_data.get(pole_smiles)
+    pole_nazwa = cleaned_data.get('pole_nazwa')
+    pole_smiles = cleaned_data.get('pole_smiles')
     
     if pole_nazwa == "" and pole_smiles == "":  #brak nazwy i smiles
         self.add_error('pole_nazwa','podaj dane')

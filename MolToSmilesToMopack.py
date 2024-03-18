f2d0faaa05498ba7ca9f9640e26f7d66c1fc9fc9
kakaovychlebicek1 <157245284+kakaovychlebicek1@users.noreply.github.com>
2024-03-18 16:24:00 +0100

    Update MolToSmilesToMopack.py

--- MolToSmilesToMopack.py
+++ MolToSmilesToMopack.py
@@ -1,11 +1,30 @@
 from urllib.request import urlopen
 from urllib.parse import quote
 
-def CIRconvert(ids):
+from urllib.request import urlopen
+from urllib.parse import quote
+
+#Punkt_1
+# 2 pola tekstowe z nazwa czasteczki smiles
+def clean(self):
+    cleaned_data = super(self).clean()
+    mol_name = cleaned_data.get("NZAWA CZASTECZKI")
+    smile_code = cleaned_data.get("KOD SMILES")
+    identifiers = input()
+
     try:
         url = 'http://cactus.nci.nih.gov/chemical/structure/' + quote(ids) + '/smiles' 
         ans = urlopen(url).read().decode('utf8')
-        for line in ans:
-            return ans
+        return ans
     except:
-        return 'Did not work'
+        self.add_error('Blad.')
+#        return 'Did not work'
+
+#Punkt_2
+# Komunikat, przy enterze, ze pole jest puste
+
+#Punkt_3
+# Funkcja do konwersji
+
+
+#Punkt_4

16080c32730145e4f55808fcc4d8ab688865b247
tugnt12 <153234784+tugnt12@users.noreply.github.com>
2024-03-25 17:55:05 +0100

    Update Utilities.py

--- abecadlo/blog/Utilities.py
+++ abecadlo/blog/Utilities.py
@@ -1,11 +1,12 @@
 def make_png_and_mop(smiles, id):
     import openbabel.pybel
     import os
+    from django.conf import settings
     czasteczka = openbabel.pybel.readstring("smi", smiles)
-    os.mkdir(str(id))
-    czasteczka.write(format="_png2",filename=str(id)+"/molecule.png", overwrite=True)
+    os.mkdir(settings.MEDIA_ROOT+'/'+str(id))
+    czasteczka.write(format="_png2",filename=settings.MEDIA_ROOT+'/'+str(id)+"/molecule.png", overwrite=True)
     czasteczka.make3D()
-    czasteczka.write(format="mop",filename=str(id)+"/molecule.mop",overwrite=True)
+    czasteczka.write(format="mop",filename=settings.MEDIA_ROOT+'/'+str(id)+"/molecule.mop",overwrite=True)
 
 def smile_check(smiles):
     import openbabel.pybel

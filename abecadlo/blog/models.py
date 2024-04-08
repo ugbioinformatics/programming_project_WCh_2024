f98f55567f6ab03e5da293e12dd40ebb40628287
tugnt12 <153234784+tugnt12@users.noreply.github.com>
2024-04-08 16:48:15 +0200

    Update models.py

--- abecadlo/blog/models.py
+++ abecadlo/blog/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
- #   author = models.ForeignKey(
- #       "auth.User",
- #       on_delete=models.CASCADE,
- #   )
+    author = models.ForeignKey(User,on_delete=models.CASCADE,blank = True,null = True)
     author = models.CharField(max_length=20)
     nazwa = models.TextField()
     smiles = models.TextField()

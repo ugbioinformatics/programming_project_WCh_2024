7e70953c937e0593b13ec72cabb74b97943996fa
Karolina-Sledz <152807356+Karolina-Sledz@users.noreply.github.com>
2024-04-15 15:48:10 +0200

    Update urls.py

--- ch6-blog-app-with-forms/django_project/urls.py
+++ ch6-blog-app-with-forms/django_project/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include  # new
+from django.urls import path, include 
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),  # new
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", include("blog.urls")), 
 ]

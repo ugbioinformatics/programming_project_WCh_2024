6771b433a1cceada9de5b0a42d9380fbbc3be441
marbac12 <153139255+marbac12@users.noreply.github.com>
2024-04-15 16:20:20 +0200

    Update views.py

--- abecadlo/accounts/views.py
+++ abecadlo/accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import rende
+from django.urls import path
+from .views import SignUpView
 
-# Create your views here.
+
+urlpatterns = [
+    path("signup/", SignUpView.as_view(), name="signup"),
+]

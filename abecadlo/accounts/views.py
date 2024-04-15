eb51ab3d5f71a30da4fd403685c7dd12ca2f7b1d
marbac12 <153139255+marbac12@users.noreply.github.com>
2024-04-15 16:21:14 +0200

    Update views.py

--- abecadlo/accounts/views.py
+++ abecadlo/accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import rende
-from django.urls import path
-from .views import SignUpView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
-urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
-]
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
